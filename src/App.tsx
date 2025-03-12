8f32b6b6da6f4a8f2813fb5d601b4c600cd7d257
osama seid <osaseid1@gmail.com>
2025-03-12 14:41:42 +0300

    file structured

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='bg-gradient-to-r from-blue-100 to-blue-300'>Hello Osama i Update the basic vite config</h1>
+    
+    </>
+  )
+}
+
+export default App
